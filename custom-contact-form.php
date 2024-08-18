0626c82e942d3758e75473f93868c8a8896d2778
MaloGerard01 <malo.gerard35@gmail.com>
2024-08-18 16:11:31 +0200

    added name to plugin info

--- custom-contact-form.php
+++ custom-contact-form.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Contact Form
 Description: A custom contact form plugin.
 Version: 1.0
-Author: Votre Nom
+Author: Malo Gérard
 */
 
 // Sécurité: Empêcher un accès direct au fichier
@@ -11,11 +11,9 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Inclure les fichiers nécessaires
 include_once plugin_dir_path(__FILE__) . 'includes/install.php';
 include_once plugin_dir_path(__FILE__) . 'includes/form-handler.php';
 include_once plugin_dir_path(__FILE__) . 'includes/admin-page.php';
 
-// Enregistrer l'activation du plugin
 register_activation_hook(__FILE__, 'cc_create_database_table');
 ?>
--- includes/js/cc-recaptcha.js
+++ includes/js/cc-recaptcha.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     grecaptcha.ready(function () {
         var siteKey = cc_recaptcha.siteKey;
-        console.log(siteKey);
         
-        // Exécuter reCAPTCHA avec le site key passé par PHP
         grecaptcha.execute(siteKey, { action: 'submit' }).then(function (token) {
             document.getElementById('g-recaptcha-response').value = token;
         });
